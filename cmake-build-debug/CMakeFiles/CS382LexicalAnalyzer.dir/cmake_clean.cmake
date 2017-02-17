36a5cbd35b50aa6aa1e9be8b06ec136a039ce2a1
Aaron Brzowski <aaron.brzowski@gmail.com>
2017-02-17 10:40:01 -0600

    initialization

---
+++ cmake-build-debug/CMakeFiles/CS382LexicalAnalyzer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS382LexicalAnalyzer.dir/main.cpp.o"
+  "CS382LexicalAnalyzer.pdb"
+  "CS382LexicalAnalyzer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS382LexicalAnalyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
